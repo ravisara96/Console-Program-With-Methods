455ffc2aa0f372ab41cf35236dc8675964c5d9a8
ravis <ravis@DESKTOP-MO95S8H>
2020-09-23 17:21:17 +0530

    Add project files.

---
+++ Console Programe With Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Programe_With_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
